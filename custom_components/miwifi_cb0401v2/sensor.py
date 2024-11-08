c81f4c9d349d4b832e79bf6315369cad3682cabd
MKsys1337 <33372834+MKsys1337@users.noreply.github.com>
2024-11-08 21:12:17 +0100

    Update sensor.py

--- custom_components/miwifi_cb0401v2/sensor.py
+++ custom_components/miwifi_cb0401v2/sensor.py
@@ -37,11 +37,13 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_e
     client = hass.data[DOMAIN][entry.entry_id]
     data_cache = DataCache(client)
 
-    sensors = [
-        BaseMiWiFiSensor(client, data_cache, "net.ipv6info.ip6addr", "IPv6 Address", icon="mdi:ip"),
-        BaseMiWiFiSensor(client, data_cache, "net.ipv6info.dns", "IPv6 DNS", icon="mdi:dns"),
-        BaseMiWiFiSensor(client, data_cache, "net.ipv4info.ipv4", "IPv4 Address", icon="mdi:ip"),
-        BaseMiWiFiSensor(client, data_cache, "net.ipv4info.dns", "IPv4 DNS", icon="mdi:dns"),
+    # Add both general and specific sensors
+    sensors = create_general_sensors(client, data_cache) + create_specific_sensors(client, data_cache)
+    async_add_entities(sensors, True)
+
+def create_general_sensors(client, data_cache):
+    """Define the list of general sensors."""
+    return [
         BaseMiWiFiSensor(client, data_cache, "net.info.cell_band", "Cell Band", icon="mdi:satellite"),
         BaseMiWiFiSensor(client, data_cache, "net.info.cell_band_5g", "Cell Band 5G", icon="mdi:satellite-variant"),
         BaseMiWiFiSensor(client, data_cache, "net.info.ci", "Cell ID", icon="mdi:map-marker"),
@@ -56,16 +58,36 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_e
         BaseMiWiFiSensor(client, data_cache, "net.info.snr", "SNR", native_unit=SIGNAL_STRENGTH_DECIBELS, icon="mdi:signal", state_class=SensorStateClass.MEASUREMENT),
         BaseMiWiFiSensor(client, data_cache, "net.info.snr_5g", "SNR 5G", native_unit=SIGNAL_STRENGTH_DECIBELS, icon="mdi:signal-variant", state_class=SensorStateClass.MEASUREMENT),
     ]
-    async_add_entities(sensors, True)
+
+def create_specific_sensors(client, data_cache):
+    """Define additional specific sensors for IP, mask, and DNS entries."""
+    return [
+        # IPv4 Address and Mask
+        BaseMiWiFiSensor(client, data_cache, "net.ipv4info.ipv4", "IPv4 Address", data_path="ip", icon="mdi:ip"),
+        BaseMiWiFiSensor(client, data_cache, "net.ipv4info.ipv4", "IPv4 Netmask", data_path="mask", icon="mdi:ip"),
+
+        # IPv4 DNS
+        BaseMiWiFiSensor(client, data_cache, "net.ipv4info.dns", "IPv4 DNS 1", data_path=0, icon="mdi:dns"),
+        BaseMiWiFiSensor(client, data_cache, "net.ipv4info.dns", "IPv4 DNS 2", data_path=1, icon="mdi:dns"),
+
+        # IPv6 Address and Mask
+        BaseMiWiFiSensor(client, data_cache, "net.ipv6info.ip6addr", "IPv6 Address", data_path="ip", icon="mdi:ip"),
+        BaseMiWiFiSensor(client, data_cache, "net.ipv6info.ip6addr", "IPv6 Netmask", data_path="mask", icon="mdi:ip"),
+
+        # IPv6 DNS
+        BaseMiWiFiSensor(client, data_cache, "net.ipv6info.dns", "IPv6 DNS 1", data_path=0, icon="mdi:dns"),
+        BaseMiWiFiSensor(client, data_cache, "net.ipv6info.dns", "IPv6 DNS 2", data_path=1, icon="mdi:dns"),
+    ]
 
 class BaseMiWiFiSensor(SensorEntity):
     """Base class for all MiWiFi sensors."""
 
-    def __init__(self, client, data_cache, sensor_key, name, native_unit=None, device_class=None, icon=None, state_class=None):
+    def __init__(self, client, data_cache, sensor_key, name, data_path=None, native_unit=None, device_class=None, icon=None, state_class=None):
         self._client = client
         self._data_cache = data_cache
         self._sensor_key = sensor_key
         self._name = name
+        self._data_path = data_path
         self._native_unit_of_measurement = native_unit
         self._device_class = device_class
         self._icon = icon
@@ -82,69 +104,65 @@ class BaseMiWiFiSensor(SensorEntity):
             "name": f"Xiaomi Router {self._client._host}",
             "manufacturer": "Xiaomi",
             "model": "CB0401V2",
-            "sw_version": self._client.firmware_version,  # Extracted from init_info
+            "sw_version": self._client.firmware_version,
         }
 
     @property
     def device_class(self):
-        """Return the device class for the sensor."""
         return self._device_class
 
     @property
     def native_unit_of_measurement(self):
-        """Return the native unit of measurement."""
         return self._native_unit_of_measurement
 
     @property
     def icon(self):
-        """Return the icon for the sensor."""
         return self._icon
 
     @property
     def state_class(self):
-        """Return the state class of the sensor."""
         return self._state_class
 
     @property
     def unique_id(self):
-        """Return a unique ID for this sensor to enable UI management."""
-        return f"{self._mac_address}_{self._sensor_key.replace('.', '_')}"
+        """Generate a unique ID for each sensor based on its sensor key and data path."""
+        # Create a base ID using the MAC address and the sensor key
+        base_id = f"{self._mac_address}_{self._sensor_key.replace('.', '_')}"
+        
+        # Append data_path information if available (e.g., 'ip', 'mask', or index for DNS)
+        if self._data_path is not None:
+            unique_suffix = str(self._data_path) if isinstance(self._data_path, int) else self._data_path
+            return f"{base_id}_{unique_suffix}"
+        
+        # Return the base ID if no data_path is specified
+        return base_id
 
     @property
     def name(self):
-        """Return the name of the sensor."""
         return self._name
 
     @property
     def state(self):
-        """Return the state of the sensor."""
         return self._state
 
     @property
     def available(self):
-        """Return True if sensor is available."""
         return self._available
-
-    @property
-    def should_poll(self):
-        """Return True if the entity should be polled."""
-        return True
 
     async def async_update(self):
         """Fetch data from the cache and update the sensor state."""
         data = await self._data_cache.get_data()
-        
+
         keys = self._sensor_key.split(".")
         value = data
         for key in keys:
             value = value.get(key, {})
 
-        if isinstance(value, list) and len(value) == 1 and isinstance(value[0], dict):
-            value = value[0]
-        
-        if value:
-            self._state = value
-            self._available = True
-        else:
-            self._state = None
-            self._available = False
+        # Extract specific data if data_path is defined
+        if isinstance(value, dict) and self._data_path:
+            value = value.get(self._data_path)
+        elif isinstance(value, list) and isinstance(self._data_path, int) and self._data_path < len(value):
+            value = value[self._data_path]
+
+        self._state = value if value is not None else None
+        self._available = value is not None
